89c303d99c29e417cb867c016e7e09790b677455
Faraphel <rc60650@hotmail.com>
2022-05-25 13:36:42 +0200

    only convert to thumbnail track that don't already have been
    Signed-off-by: Faraphel <rc60650@hotmail.com>

--- thumbnail from map.py
+++ thumbnail from map.py
@@ -4,5 +4,5 @@ import os
 
 os.makedirs("./thumbnail/", exist_ok=True)
 for filename in os.listdir("./map/"):
-    print(filename)
+    if os.path.exists(f"./thumbnail/{filename}"): continue
     Image.open(f"./map/{filename}").resize((480, 270)).save(f"./thumbnail/{filename}")
